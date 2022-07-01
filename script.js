1ff9915bc87106e888f4f58b6ffc80c3a3df0857
Jomar A. França <jomar.almeida@gmail.com>
2022-07-01 03:27:44 -0300

    Ajustando a estilização

--- script.js
+++ script.js
@@ -1,18 +1,25 @@
 
-function botao() {
-    // event.preventDefault();
+function botao(event) {
+    console.log("evento: ", event)
+    event.preventDefault();
     var fnome = document.getElementById("fname").value;
     var snome = document.getElementById("fname2").value;
     var carta = document.getElementById("femail").value;
     var dev = document.getElementById("curso").value;
-    var exp = document.getElementById("exper").value;
+    var exp = document.getElementById("exp1").value;
     var esp = document.getElementById("rad").value;
 
-    document.write ("Nome: " + fnome + " " + snome + "<br>");
-    document.write ("Email: " + carta + "<br>");
-    document.write ("Curso: " + dev + "<br>");
-    document.write ("Experiência: " + exp + "<br>");
-    document.write ("Especialização: " + esp + "<br>");
-    console.log(document.getElementById(carta));
+    console.log("qualquer coisa", document.getElementsByName("exper"));
+
+    document.getElementById("resultado").style.display='block';
+    document.getElementById("nome").innerHTML = "Nome: " + fnome + " " + snome;
+    document.getElementById("resultado").style.display='block';
+    document.getElementById("mail").innerHTML = "Email: " + carta;
+    document.getElementById("resultado").style.display='block';
+    document.getElementById("fcurso").innerHTML = "Experiência: " + dev;
+    document.getElementById("resultado").style.display='block';
+    document.getElementById("experir").innerHTML = "Curso: " + exp;
+    document.getElementById("resultado").style.display='block';
+    document.getElementById("conhecimento").innerHTML = "Especialidade: " + esp;
 }
 
