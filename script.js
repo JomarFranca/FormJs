283b3c7a022c176074aef4f9c0bbe2c0c89cbfcb
Jomar A. França <jomar.almeida@gmail.com>
2022-06-30 23:59:56 -0300

    Criando Style.css e ajustando as tags

--- script.js
+++ script.js
@@ -5,13 +5,14 @@ function botao() {
     var snome = document.getElementById("fname2").value;
     var carta = document.getElementById("femail").value;
     var dev = document.getElementById("curso").value;
-    var exp = document.getElementById("checkbox").value;
+    var exp = document.getElementById("exper").value;
     var esp = document.getElementById("rad").value;
 
-    document.write ("Nome:" + fnome + " " + snome + "<br>");
+    document.write ("Nome: " + fnome + " " + snome + "<br>");
     document.write ("Email: " + carta + "<br>");
-    document.write ("Curso:" + dev + "<br>");
+    document.write ("Curso: " + dev + "<br>");
     document.write ("Experiência: " + exp + "<br>");
     document.write ("Especialização: " + esp + "<br>");
+    console.log(document.getElementById(carta));
 }
 
