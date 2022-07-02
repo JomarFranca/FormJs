d370f8248c8f51a313991b560427018c8364669b
Jomar A. França <jomar.almeida@gmail.com>
2022-07-02 03:39:05 -0300

    Adicionando o input do tipo "radio"!

--- script.js
+++ script.js
@@ -1,25 +1,39 @@
 
 function botao(event) {
-    console.log("evento: ", event)
+    // console.log("evento: ", event)
     event.preventDefault();
+
     var fnome = document.getElementById("fname").value;
     var snome = document.getElementById("fname2").value;
+
+        document.getElementById("resultado").style.display='block';
+            document.getElementById("nome").innerHTML = "Nome: " + fnome + " " + snome;
+
     var carta = document.getElementById("femail").value;
+        document.getElementById("resultado").style.display='block';
+            document.getElementById("mail").innerHTML = "Email: " + carta;
+
     var dev = document.getElementById("curso").value;
+        document.getElementById("resultado").style.display='block';
+            document.getElementById("fcurso").innerHTML = "Experiência: " + dev;
+            // console.log("qualquer coisa", document.getElementsByName("exper"));
+
+
     var exp = document.getElementById("exp1").value;
-    var esp = document.getElementById("rad").value;
+        document.getElementById("resultado").style.display='block';
+            document.getElementById("experir").innerHTML = "Curso: " + exp;
 
-    console.log("qualquer coisa", document.getElementsByName("exper"));
 
-    document.getElementById("resultado").style.display='block';
-    document.getElementById("nome").innerHTML = "Nome: " + fnome + " " + snome;
-    document.getElementById("resultado").style.display='block';
-    document.getElementById("mail").innerHTML = "Email: " + carta;
-    document.getElementById("resultado").style.display='block';
-    document.getElementById("fcurso").innerHTML = "Experiência: " + dev;
-    document.getElementById("resultado").style.display='block';
-    document.getElementById("experir").innerHTML = "Curso: " + exp;
-    document.getElementById("resultado").style.display='block';
-    document.getElementById("conhecimento").innerHTML = "Especialidade: " + esp;
-}
-
+    var esp = document.getElementsByName("devweb");
+    var txt = "";
+    var i;
+        for(i=0; i<esp.length;i++){
+            if(esp[i].checked) {
+                txt = txt + esp[i].value + " ";
+            }
+        }
+        console.log("porra ", esp);
+        console.log("caralho ", txt);
+            document.getElementById("resultado").style.display='block';
+            document.getElementById("conhecimento").innerHTML = "Especialidade: " + txt;
+};
